9afab2be852bcfacc91d78b181e1c11e54dc897a
Jan Donyada <14304570+jandonyada@users.noreply.github.com>
2024-12-05 21:03:16 +0800

    fix for suspense

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,76 +1,16 @@
-"use client";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 
-import { useEffect, useState, Suspense } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+const EditPrompt = dynamic(() => import("@components/EditPrompt"), {
+  suspense: true,
+});
 
-import Form from "@components/Form";
-
-const UpdatePromptContent = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
-
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-  const [submitting, setIsSubmitting] = useState(false);
-
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      if (!promptId) return;
-
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    };
-
-    getPromptDetails();
-  }, [promptId]);
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    if (!promptId) return alert("Missing PromptId!");
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
+const Page = () => {
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
-  );
-};
-
-const UpdatePrompt = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <UpdatePromptContent />
+    <Suspense>
+      <EditPrompt />
     </Suspense>
   );
 };
 
-export default UpdatePrompt;
+export default Page;
